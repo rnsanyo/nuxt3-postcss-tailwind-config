1c81abf2619ce359ee54f696af8b9838a4db08d9
Sanchez Roberto <Sanchiesu.Roberuto@exc.epson.co.jp>
2023-07-03 13:45:16 +0900

    first commit

---
+++ app.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="main-div">
+    <h1>Page Title</h1>
+    <h2>Page subtitle 2</h2>
+    <h3>Page subtitle 3</h3>
+  </div>
+</template>
+
+<style lang="postcss">
+.main-div {
+  h1 {
+    @apply !text-5xl md:!text-6xl max-w-[600px];
+  }
+  h2 {
+    @apply text-red-300
+  }
+}
+</style>
